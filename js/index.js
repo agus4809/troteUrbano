8a828d772eb1c2169635a26780b0a0fc02aad2c0
augusto Garcia <augusto4809@gmail.com>
2022-08-25 18:31:24 -0300

    primer commit trote urbano

---
+++ js/index.js
@@ -0,0 +1,17 @@
+let juego = confirm("Hola, Vamos a jugar un juego. Yo voy a pensar un nro aleatorio entre 1 y 10 y vos tenes que adivinarlo. ¿Te animas?");
+let numero=Math.floor(Math.random() * 10);
+console.log(numero)       
+let respuesta;
+if (juego) {
+    do{
+        respuesta= parseInt(prompt("Ingrese un numero para jugar"));
+        if(numero === respuesta){
+            alert("usted ha ganado");
+         } else{
+                respuesta = alert("ha fallado")
+            }
+    }while(respuesta !=numero);
+    alert("Gracias por jugar")
+}else{
+    alert("ok, gracias por nada");
+}
